f77678563623a7c3dc0814f6925e26cb04117f0f
Daniele Mingolla <33353183+danielemingolla@users.noreply.github.com>
2019-08-11 10:28:55 +0200

    Entirely redesign and changed domain

---
+++ assets/js/form.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  $("#contact-form").validate({
+    messages: {
+      name: {
+        required: "Please enter your name" //You can customise this message
+      },
+      email: {
+        required: "Please enter your email" //You can customise this message
+      },
+      subject: {
+        required: "Please enter your subject" //You can customise this message
+      },
+      message: {
+        required: "Please enter your message" //You can customise this message
+      }
+    }
+  });
+  $("#emailsubmit").click(function(event) {
+    event.preventDefault();
+    if ($("#contact-form").valid()) {
+      $(location).attr(
+        "href",
+        "mailto:danielemingolla98ta@gmail.com?subject=" +
+          encodeURIComponent($("#csubject").val()) +
+          "&body=" +
+          encodeURIComponent($("#cmessage").val())
+      );
+    }
+  });
+});
